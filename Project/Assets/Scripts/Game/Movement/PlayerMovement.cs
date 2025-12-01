0fd66b4b6610a6cdfd10e0fe721401721323a9e1
Anton Bryzghalov <absamurai@gmail.com>
2025-12-02 01:01:06 +0200

    Some movement refactoring reverted.

--- Project/Assets/Scripts/Game/Movement/PlayerMovement.cs
+++ Project/Assets/Scripts/Game/Movement/PlayerMovement.cs
@@ -32,23 +32,20 @@ namespace TowerDefence.Game.Movement
         {
             var deltaTime = Time.fixedDeltaTime;
             Vector3 displacement = _impulse * deltaTime;
-            // Impulse fading
-            _impulse = Vector3.Lerp(_impulse, Vector3.zero, impulseDragFactor * deltaTime);
+            var hasNotMoveInput = Mathf.Approximately(_moveInput.sqrMagnitude, 0f);
+            if (hasNotMoveInput && Mathf.Approximately(displacement.sqrMagnitude, 0f)) return;
 
             var targetPosition = rigidBody.position + displacement + _moveInput * (movementSpeed * deltaTime);
             rigidBody.MovePosition(targetPosition);
-
-            if (Mathf.Approximately(_moveInput.sqrMagnitude, 0f)) return;
-            var targetRotation = Quaternion.LookRotation(_moveInput);
-            rigidBody.MoveRotation(Quaternion.Slerp(rigidBody.rotation, targetRotation, rotationSpeed * Time.deltaTime));
+            _impulse = Vector3.Lerp(_impulse, Vector3.zero, impulseDragFactor * deltaTime);
         }
 
-        /*private void LateUpdate()
+        private void LateUpdate()
         {
             if (Mathf.Approximately(_moveInput.sqrMagnitude, 0f)) return;
 
             viewTransform.localRotation =
                 Quaternion.Slerp(viewTransform.localRotation, Quaternion.LookRotation(_moveInput), rotationSpeed * Time.deltaTime);
-        }*/
+        }
     }
 }
